b11b11d594baaec5e30dc1a85aa1bfe7f42c9729
Folenr <t.a.r252e@gmail.com>
2026-04-16 13:29:59 +0300

    added Secant method

--- methods.py
+++ methods.py
@@ -29,9 +29,19 @@ class Methods:
             return p
         p = p - f(p)/g(p)
         return Methods.newton(f, g, p, tol)
+    
+    def secant(f,p1,p2,tol):
+        if np.abs(f(p1)) < tol:
+            return p1
+        temp = p1
+        p1 = p1 - (f(p1)*(p1-p2))/(f(p1)-f(p2))
+        p2 = temp
+        return Methods.secant(f, p1, p2, tol)
 
 f = lambda x: x**3 + 4*x**2 - 10
 g = lambda x: 0.5*(10-x**3)**0.5
+
 print(Methods.biSection(f,0,2,0.00000000001))
 print(Methods.fixedPoint(f,g,1.5,0.00000000001))
 print(Methods.newton(f,None,1.5,0.00000000001))
+print(Methods.secant(f,2,1,0.00000000001))
